1cbb4afb43593b1cbf49683af0c23cd106dc9624
bhanuvinay.WT <bhanu.vinay@walkingtree.tech>
2021-07-12 12:11:29 +0530

    step -3

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -6,60 +6,64 @@ import java.util.Collections;
 import java.util.List;
 public class Main {
 
-    public static void main(String[] args) {
-        long pseudoRandomNumber = System.nanoTime();
-        Scanner scanner = new Scanner(System.in);
-        int value = scanner.nextInt();
-        if(value > 9)
-            System.out.println("Error");
-        else
-        System.out.println("The random secret number is "+randomGenerator(value));
-    }
-    public static String randomGenerator(int length) {
-        List<Integer> randomList = new ArrayList<>(List.of( 2, 3, 4, 5, 6, 7, 8, 9));
-        Collections.shuffle(randomList);
-        StringBuilder result = new StringBuilder();
-        for (var ch : randomList.subList(0, length)) {
-            result.append(ch);
-        }
-        return result.toString();
-    }
-}
-          /*  int original = 9305;
-            int[] o = convertingToArray(original);
-            int[] a = convertingToArray(value);
-            for (int i = 0; i < 4; i++) {
-                a[i] = value % 10;
-                value = value / 10;
+
+        public static int length = 0;
+
+        public static void main(String[] args) {
+            long pseudoRandomNumber = System.nanoTime();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please, enter the secret code's length:");
+            length = scanner.nextInt();
+            if(length > 9){
+                System.out.println("error");
+                System.out.println("Please, enter the secret code's length:");
+                length = scanner.nextInt();
             }
-            int cows = checkingForCows(o, a);
-            int bulls = checkingForBulls(o, a);
-            cows = cows - bulls;
-            if (cows > 0 && bulls > 0)
-                System.out.print(bulls + " bull(s) and " + cows + " cow(s).");
-            else if (cows > 0)
-                System.out.print(cows + " cow(s).");
-            else if (bulls > 0)
-                System.out.print(bulls + " bull(s).");
-            else
-                System.out.print("None.");
-            System.out.print("The secret code is 9305.");
-            scanner.close();
+            String original = randomGenerator(length);
+            System.out.println(original);
+            System.out.println("Okay, let's start a game!");
+            int i = 0;
+            while (true) {
+                System.out.println("Turn " + (++i));
+                String verify = scanner.next();
+                char[] o = original.toCharArray();
+                char[] a = verify.toCharArray();
+                int cows = checkingForCows(o, a);
+                int bulls = checkingForBulls(o, a);
+                cows = cows - bulls;
+                if (cows > 0 && bulls > 0)
+                    System.out.println(bulls + " bulls and " + cows + " cow");
+                else if (cows > 0)
+                    System.out.println(cows + " cows.");
+                else if (cows == 1 && bulls == 1)
+                    System.out.println(bulls + " bull and " + cows + " cow");
+                else if (cows > 1 && bulls == 1)
+                    System.out.println(bulls + " bull and " + cows + " cows");
+                else if (bulls == length) {
+                    System.out.println(bulls + " bulls");
+                    System.out.println("Congratulations! You guessed the secret code.");
+                    break;
+                } else {
+                    System.out.println(bulls + " bulls");
+                }
+            }
+
         }
 
-        public static int[] convertingToArray(int value) {
-            int[] a = new int[4];
-            for (int i = 0; i < 4; i++) {
-                a[i] = value % 10;
-                value = value / 10;
+        public static String randomGenerator(int length) {
+            List<Integer> randomList = new ArrayList<>(List.of(2, 3, 4, 5, 6, 7, 8, 9));
+            Collections.shuffle(randomList);
+            StringBuilder result = new StringBuilder();
+            for (var ch : randomList.subList(0, length)) {
+                result.append(ch);
             }
-            return a;
+            return result.toString();
         }
 
-        public static int checkingForCows(int[] o, int[] a) {
+        public static int checkingForCows(char[] o, char[] a) {
             int count = 0;
-            for (int i = 0; i < 4; i++)
-                for (int j = 0; j < 4; j++)
+            for (int i = 0; i < length; i++)
+                for (int j = 0; j < length; j++)
                     if (o[i] == a[j])
                         count++;
 
@@ -69,19 +73,16 @@ public class Main {
                 return count;
         }
 
-        public static int checkingForBulls(int[] o, int[] a) {
+        public static int checkingForBulls(char[] o, char[] a) {
             int count = 0;
-            for (int i = 0; i < 4; i++)
-                for (int j = 0; j < 4; j++)
+            for (int i = 0; i < length; i++)
+                for (int j = 0; j < length; j++)
                     if (o[i] == a[j] && i == j)
                         count++;
             if (count == 0)
                 return 0;
             else
                 return count;
-        }*/
+        }
 
-
-
-
-
+    }
