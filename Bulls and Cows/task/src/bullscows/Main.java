e88013ed5ec8d92c2f2791d8876f096328503278
bhanuvinay.WT <bhanu.vinay@walkingtree.tech>
2021-07-13 16:13:48 +0530

    step-7

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -8,50 +8,70 @@ public class Main {
     public static String s1 = "0123456789abcdefghijklmnopqrstuvwxyz";
 
     public static void main(String[] args) {
+        // long pseudoRandomNumber = System.nanoTime();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please, enter the secret code's length:");
-        length = scanner.nextInt();
-        System.out.println("Input the number of possible symbols in the code:");
-        symbolsLength = scanner.nextInt();
-        String original = generateRandomDigits();
-        String x="";
-        for(int i=0;i<length;i++){
-            x=x+"*";
-        }
-        //informing about the code length and values used in the code.
-        System.out.println("The secret is prepared:"+x+" (0-9,a-"+s1.charAt(symbolsLength-1)+")");
-        System.out.println("Okay, let's start a game!");
-        int i = 0;
-        while (true) {
-            System.out.println("Turn " + (++i));
-            String verify = scanner.next();
-            char[] o = original.toCharArray();
-            // System.out.println(original.length());
-            char[] a = verify.toCharArray();
-            // for(int j=0;j<a.length;j++) {
-            // System.out.println("i:" + j + " " + a[j]);
-            // } System.out.println(verify.length());
-            int cows = checkingForCows(o, a);
-            int bulls = checkingForBulls(o, a);
-            cows = cows - bulls;
-            if (cows > 0 && bulls > 0)
-                System.out.println(bulls + " bulls and " + cows + " cow");
-            else if (cows > 0)
-                System.out.println(cows + " cows");
-            else if (cows == 1 && bulls < 0)
-                System.out.println(cows + " cow");
-            else if (cows == 1 && bulls == 1)
-                System.out.println(bulls + " bull and " + cows + " cow");
-            else if (cows > 1 && bulls == 1)
-                System.out.println(bulls + " bull and " + cows + " cows");
-            else if (bulls == length) {
-                System.out.println(bulls + " bulls");
-                System.out.println("Congratulations! You guessed the secret code.");
-                break;
-            } else {
-                System.out.println(bulls + " bulls");
+        try {
+            System.out.println("Please, enter the secret code's length:");
+            length = scanner.nextInt();
+
+            System.out.println("Input the number of possible symbols in the code:");
+            symbolsLength = scanner.nextInt();
+
+            if(length == 0) {
+                System.out.println("Error: it's not possible to generate a code with a length of " + length + " with "
+                        + symbolsLength + "  unique symbols.");
             }
+            else if (length > symbolsLength) {
+                System.out.println("Error: it's not possible to generate a code with a length of " + length + " with "
+                        + symbolsLength + "  unique symbols.");
+            }
+            else if (symbolsLength > 36) {
+                System.out.println("Error: maximum number of possible symbols in the code is 36 (0-9, a-z).");
+            } else {
+                String original = generateRandomDigits();
+                String x = "";
+                for (int i = 0; i < length; i++) {
+                    x = x + "*";
+                }
+                // informing about the code length and values used in the code.
+                System.out.println("The secret is prepared:" + x + " (0-9,a-" + s1.charAt(symbolsLength - 1) + ")");
+                System.out.println("Okay, let's start a game!");
+                int i = 0;
+                while (true) {
+                    System.out.println("Turn " + (++i));
+                    String verify = scanner.next();
+                    char[] o = original.toCharArray();
+                    // System.out.println(original.length());
+                    char[] a = verify.toCharArray();
+                    // for(int j=0;j<a.length;j++) {
+                    // System.out.println("i:" + j + " " + a[j]);
+                    // } System.out.println(verify.length());
+                    int cows = checkingForCows(o, a);
+                    int bulls = checkingForBulls(o, a);
+                    cows = cows - bulls;
+                    if (cows > 0 && bulls > 0)
+                        System.out.println(bulls + " bulls and " + cows + " cow");
+                    else if (cows > 0)
+                        System.out.println(cows + " cows");
+                    else if (cows == 1 && bulls < 0)
+                        System.out.println(cows + " cow");
+                    else if (cows == 1 && bulls == 1)
+                        System.out.println(bulls + " bull and " + cows + " cow");
+                    else if (cows > 1 && bulls == 1)
+                        System.out.println(bulls + " bull and " + cows + " cows");
+                    else if (bulls == length) {
+                        System.out.println(bulls + " bulls");
+                        System.out.println("Congratulations! You guessed the secret code.");
+                        break;
+                    } else {
+                        System.out.println(bulls + " bulls");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + length + " isn't a valid number");
         }
+
         scanner.close();
 
     }
@@ -60,7 +80,7 @@ public class Main {
     public static String generateRandomDigits() {
         ArrayList<Character> list = new ArrayList<Character>();
         Random random = new Random();
-
+        String s1 = "0123456789abcdefghijklmnopqrstuvwxyz";
         String s = "";
         while (true) {
             char value = s1.charAt(random.nextInt(symbolsLength));
@@ -73,7 +93,7 @@ public class Main {
         }
         return s;
     }
-//counting no of cows in the given input
+
     public static int checkingForCows(char[] o, char[] a) {
         int count = 0;
         for (int i = 0; i < o.length; i++)
@@ -88,7 +108,7 @@ public class Main {
         else
             return count;
     }
-//counting no of bulls in the given input
+
     public static int checkingForBulls(char[] o, char[] a) {
         int count = 0;
         for (int i = 0; i < o.length; i++)
